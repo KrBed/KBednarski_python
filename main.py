a9f9612767ac60dccece6f60e15cf2fb3d4fa52f
Hubert Koloska <szkolny98@wp.pl>
2022-02-26 12:40:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+
+print(sys.version)
+
+if 5 > 2 or 4 > 1:
+    print('OK')
+
+x = str('3')
+print(x)
+print(type(x))
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+x = 5
+y = 10
+print(x + y)
+x = "awesome"
+
+
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+
+
+myfunc()
+
+print("Python is " + x)
